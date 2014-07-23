d61e3a8776c5fc84cb0898964d323e4773ca1df8
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2014-07-23 11:55:53 -0430

    gravatar feature

--- projects/emberBook/js/app.js
+++ projects/emberBook/js/app.js
@@ -1,1 +1,11 @@
 var MovieTracker = Ember.Application.create();
+
+MovieTracker.GravatarImageComponent = Ember.Component.extend({
+	size: 100,
+	email: '',
+	gravatarUrl: function() {
+		var email = this.get('email'),
+		    size = this.get('size');
+		return 'http://www.gravatar.com/avatar/' + hex_md5(email);
+	}.property('email', 'size')
+});
