badbd1d79c11a6ed74a0263c2c159781b1edeaf9
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2015-10-21 17:18:58 -0300

    change some files to follow best practices

--- projects/angularForms/src/app/js/directiveFormatMoney.js
+++ projects/angularForms/src/app/js/directiveFormatMoney.js
@@ -2,23 +2,26 @@
 (function () {
   'use strict';
 
-  angular.module('greyForms.directiveFormatMoney', [])
-    .directive('formatMoney', ['$filter', function ($filter) {
-      return {
-        require: '?ngModel',
-        link: function (scope, elem, attrs, ctrl) {
-          if (!ctrl) {
-            return;
-          }
-          ctrl.$parsers.unshift(function (value) {
-            elem.maskMoney();
-            return elem[0].value;
-          });
-          ctrl.$formatters.unshift(function (value) {
-            elem.maskMoney();
-            return elem[0].value;
-          });
+  angular.module('greyForms.directiveFormatMoney', []).directive('formatMoney', formatMoney);
+
+  formatMoney.$inject = ['$filter'];
+
+  function formatMoney($filter) {
+    return {
+      require: '?ngModel',
+      link: function (scope, elem, attrs, ctrl) {
+        if (!ctrl) {
+          return;
         }
-      };
-    }]);
+        ctrl.$parsers.unshift(function (value) {
+          elem.maskMoney();
+          return elem[0].value;
+        });
+        ctrl.$formatters.unshift(function (value) {
+          elem.maskMoney();
+          return elem[0].value;
+        });
+      }
+    };
+  }
 }());
--- projects/angularForms/src/app/js/main.js
+++ projects/angularForms/src/app/js/main.js
@@ -11,53 +11,54 @@
     'greyForms.directiveFormatMoney'
   ]);
 
-  angular.module('greyForms.controllerMain', [])
-    .controller('MainController', ['$scope', 'fileModel', 'saveFile',
-      function ($scope, fileModel, saveFile) {
-        $scope.model = null;
-        $scope.requestDateOpen = false;
-        $scope.authOneDateOpen = false;
-        $scope.authTwoDateOpen = false;
-        $scope.errorBoxVisible = false;
-        $scope.errorMessages = [];
+  angular.module('greyForms.controllerMain', []).controller('MainController', MainController);
 
-        function init(data) {
-          // offices and jobs are data returned from the server
-          // It is use in the typeahead (autocomplete) fields
-          $scope.jobs = data.source.jobs;
-          $scope.offices = data.source.offices;
-          $scope.model = fileModel.init(data);
+  MainController.$inject = ['$scope', 'fileModel', 'saveFile'];
+
+  function MainController($scope, fileModel, saveFile) {
+    $scope.model = null;
+    $scope.requestDateOpen = false;
+    $scope.authOneDateOpen = false;
+    $scope.authTwoDateOpen = false;
+    $scope.errorBoxVisible = false;
+    $scope.errorMessages = [];
+    $scope.wiredFormSubmit = wiredFormSubmit;
+    $scope.addExtraInfoLine = fileModel.addExtraInfoLine;
+    init(GREY_DATA);
+
+    function init(data) {
+      // offices and jobs are data returned from the server
+      // It is use in the typeahead (autocomplete) fields
+      $scope.jobs = data.source.jobs;
+      $scope.offices = data.source.offices;
+      $scope.model = fileModel.init(data);
+    }
+
+    function wiredFormSubmit() {
+      $scope.errorBoxVisible = false;
+      $scope.errorMessages = [];
+      // The use of 'else if' is to avoid producing ternary expressions
+      // that were causing errors with the gulp uglify task
+      if ($scope.wiredForm.$invalid) {
+        $scope.errorBoxVisible = true;
+        if ($scope.wiredForm.$error.required) {
+          $scope.errorMessages.push('One or more required fields are missing');
         }
-
-        function wiredFormSubmit() {
-          $scope.errorBoxVisible = false;
-          $scope.errorMessages = [];
-          // The use of 'else if' is to avoid producing ternary expressions
-          // that were causing errors with the gulp uglify task
-          if ($scope.wiredForm.$invalid) {
-            $scope.errorBoxVisible = true;
-            if ($scope.wiredForm.$error.required) {
-              $scope.errorMessages.push('One or more required fields are missing');
-            }
-            if ($scope.wiredForm.$error.pattern) {
-              $scope.errorMessages.push('One or more fields do not match its pattern');
-            }
-            if ($scope.wiredForm.$error.maxlength) {
-              $scope.errorMessages.push('One or more fields are too long');
-            }
-            if ($scope.wiredForm.$error.date) {
-              $scope.errorMessages.push('One or more fields are not valid dates');
-            }
-          } else if (!fileModel.valid()) {
-            $scope.errorBoxVisible = true;
-            $scope.errorMessages = fileModel.getErrorMessages();
-          } else {
-            saveFile.toPdf($scope.model);
-          }
+        if ($scope.wiredForm.$error.pattern) {
+          $scope.errorMessages.push('One or more fields do not match its pattern');
         }
-
-        $scope.wiredFormSubmit = wiredFormSubmit;
-        $scope.addExtraInfoLine = fileModel.addExtraInfoLine;
-        init(GREY_DATA);
-      }]);
+        if ($scope.wiredForm.$error.maxlength) {
+          $scope.errorMessages.push('One or more fields are too long');
+        }
+        if ($scope.wiredForm.$error.date) {
+          $scope.errorMessages.push('One or more fields are not valid dates');
+        }
+      } else if (!fileModel.valid()) {
+        $scope.errorBoxVisible = true;
+        $scope.errorMessages = fileModel.getErrorMessages();
+      } else {
+        saveFile.toPdf($scope.model);
+      }
+    }
+  }
 }());
