23d284d21a7925f3b91c74543e22ac91985a35c0
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2016-04-23 11:00:42 -0300

    Dislike on react getting started

--- projects/react-getting-started/src/components/index.js
+++ projects/react-getting-started/src/components/index.js
@@ -9,10 +9,15 @@ export default class App extends React.Component {
       message: 'Talk is cheap'
     }
     this.onLike = this._onLike.bind(this)
+    this.onDislike = this._onDislike.bind(this)
   }
 
   _onLike () {
     this.setState({likesCount: this.state.likesCount + 1})
+  }
+
+  _onDislike () {
+    this.setState({likesCount: this.state.likesCount - 1})
   }
 
   render () {
@@ -21,6 +26,7 @@ export default class App extends React.Component {
         <h1>Dashboard</h1>
         <h3>Customer Analytics</h3>
         <button onClick={ this.onLike }>I like this!</button>
+        <button onClick={ this.onDislike }>I don't like this!</button>
         <Likes count={ this.state.likesCount } message={ this.state.message } />
         <Likes count={ 1 } />
       </div>
