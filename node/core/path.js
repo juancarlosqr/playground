75fde16366229a917b28c4904274475920a4e9d7
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2019-10-18 20:46:12 +0200

    Add basename in node

--- node/core/path.js
+++ node/core/path.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 const filenameInfo = path.parse(__filename);
+const basenameInfo = path.basename(__dirname);
 
 console.log(filenameInfo);
+console.log(basenameInfo);
 console.log(path);
